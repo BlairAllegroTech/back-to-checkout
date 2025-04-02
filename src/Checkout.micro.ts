0eccc0071fb61a8e1aba78f8eac25f5d958fcc91
BlairJ <blair.jacobs@hypr.co.nz>
2025-04-03 10:05:56 +1300

    Scan single product

--- src/Checkout.micro.ts
+++ src/Checkout.micro.ts
@@ -2,21 +2,34 @@ import { Checkout } from "./Checkout"
 import { assertThat, match } from "mismatched"
 
 describe("Checkout", ()=>{
-    it("exists", ()=>{
-        const checkout = new Checkout()
-        assertThat(checkout).isNot(undefined)
+    let checkout: Checkout
+    beforeEach(() => {
+        checkout = new Checkout()
     })
 
-    describe("scan product", () =>{
-        let checkout: Checkout
-        beforeEach(() => {
-            checkout = new Checkout()
+    it("no products scanned", () =>{
+        assertThat(checkout.total).is(0)
+    })
+
+    describe("scan single product", () =>{
+        it("scan product A", ()=>{
+            checkout.scan("A")
+            assertThat(checkout.total).is(50)
         })
 
-        it("no products scanned", () =>{
-            assertThat(checkout.total).is(0)
+        it("scan product B", ()=>{
+            checkout.scan("B")
+            assertThat(checkout.total).is(80)
         })
 
+        it("scan product C", ()=>{
+            checkout.scan("C")
+            assertThat(checkout.total).is(20)
+        })
 
+        it("scan product D", ()=>{
+            checkout.scan("D")
+            assertThat(checkout.total).is(15)
+        })
     })
 })
--- src/Checkout.ts
+++ src/Checkout.ts
@@ -1,9 +1,10 @@
 export class Checkout {
-    total: number = 0;
+  total: number = 0;
 
-
-    scan(productSKU: string) {
-        throw new Error("Method not implemented.");
-    }
-
+  scan(productSKU: string) {
+    if (productSKU === "A") this.total = 50;
+    if (productSKU === "B") this.total = 80;
+    if (productSKU === "C") this.total = 20;
+    if (productSKU === "D") this.total = 15;
+  }
 }
